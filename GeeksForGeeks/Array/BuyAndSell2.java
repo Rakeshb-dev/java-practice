369b361d17851fdb810bcfb6370e765207cce31f
Rakeshb-dev <writetorakeshb@gmail.com>
2025-06-21 18:14:01 +0530

    buy and sell updated and add

---
+++ GeeksForGeeks/Array/BuyAndSell2.java
@@ -0,0 +1,17 @@
+package geeks.Array;
+
+public class BuyAndSell2 {
+    public static void main(String[] args) {
+             int [] arr = {1,5,3,8,12};
+        System.out.println(profit(arr));
+    }
+    public static int profit(int[]arr){
+        int profit = 0;
+        for(int i =1;i< arr.length;i++){
+            if(arr[i]>arr[i-1]){
+                profit+=(arr[i]-arr[i-1]);
+            }
+        }
+        return profit;
+    }
+}
